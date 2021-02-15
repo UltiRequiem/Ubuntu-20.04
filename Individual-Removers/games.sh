f2b475ae391026c1422dbf8121616ed2940a592f
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-02-15 13:11:59 -0500

    Update games.sh

--- Individual-Removers/games.sh
+++ Individual-Removers/games.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
-apt-get purge gnome-games-common gbrainy && sudo apt-get autoremove
+apt-get purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex
 echo "Ubuntu Games has been removed successfully."
