e57dbbdedd2d3484c028f6f933b8287b9eb540f1
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-02-21 14:22:03 -0500

    Update brave.sh

--- Individual-Installers/brave.sh
+++ Individual-Installers/brave.sh
@@ -2,9 +2,9 @@
 
 apt install apt-transport-https curl gnupg
 
-curl -s https://brave-browser-apt-release.s3.brave5t5rjjg3s6k.onion/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave5t5rjjg3s6k.onion/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
 
 apt update
 
