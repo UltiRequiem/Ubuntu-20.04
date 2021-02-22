29b8ae9e2cc966bc56e2c1e3e21d83f549cbc76a
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-02-21 21:25:33 -0500

    Create jetbrain-toolbox.sh

---
+++ Individual-Installers/jetbrain-toolbox.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.20.7940.tar.gz
+tar -xf jetbrains-toolbox-1.20.7940.tar.gz
+cd jetbrains-toolbox-1.20.7940
+chmod +x jetbrains-toolbox
+./jetbrains-toolbox
+
+echo "The program is running, this may take a few moment...."
